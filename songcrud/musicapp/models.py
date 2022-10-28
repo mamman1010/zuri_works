0fb69e06ef3d92a5a4ae42911335a2a0c87d530f
mammanten <mammanharuna@gmail.com>
2022-10-28 11:19:11 +0100

    genesis commit

---
+++ songcrud/musicapp/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+
+# Create your models here.
+class Artiste(models.Model):
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    age = models.IntegerField()
+
+
+class Song(models.Model):
+    title = models.CharField(max_length=200)
+    date_released = models.DateTimeField()
+    likes = models.IntegerField()
+    artiste_id = models.ForeignKey('Artiste', on_delete=models.CASCADE)
+
+
+class Lyric(models.Model):
+    content = models.TextField()
+    song_id = models.ForeignKey('Song', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
